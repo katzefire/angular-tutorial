e3fbb47af8cc81bcf7ac7937b74714151d2c0a4d
Tatiana Burak <tburak@megazebra.com>
2016-05-25 18:20:36 +0200

    added heroes component

--- app/app.component.js
+++ app/app.component.js
@@ -1,45 +1,24 @@
-(function(app) {
+/**
+ * Created on 25.05.16.
+ */
 
+(function(app) {
   app.AppComponent =
     ng.core.Component({
         selector: 'my-app',
-        styleUrls: ['hero-styles.css'],
         template: `
-          <h1>{{title}}</h1>
-          <h2>My Heroes</h2>
-          <ul class="heroes">
-            <li *ngFor="let hero of heroes"
-                [class.selected]="hero === selectedHero"
-                (click)="onSelect(hero)">
-              <span class="badge">{{hero.id}}</span> {{hero.name}}
-            </li>
-          </ul>
+              <h1>{{title}}</h1>
+              <my-heroes></my-heroes>
+        `,
+        directives: [app.HeroesComponent],
+        providers: [
+          app.HeroService
+        ]
 
-          <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-          `,
-        directives: [app.HeroDetailComponent],
-        providers: [app.HeroService]
-      })
+    })
       .Class({
-        constructor: [app.HeroService, function(heroService) {
-          this.title =  'Tour of Heroes';
-          this.selectedHero = null;
-          this.heroService = heroService;
-        }],
-        onSelect: function(hero) {
-          this.selectedHero = hero;
-        },
-        getHeroes: function() {
-          this.heroService.getHeroes()
-            .then(function(heroes) {
-              this.heroes = heroes;
-            }.bind(this))
-            .catch(function(error) {
-              console.log(error);
-            });
-        },
-        ngOnInit: function() {
-          this.getHeroes();
+        constructor: function() {
+          this.title = 'Tour of Heroes';
         }
       });
 
---
+++ app/heroes.component.js
@@ -0,0 +1,46 @@
+(function(app) {
+
+  app.HeroesComponent =
+    ng.core.Component({
+        selector: 'my-heroes',
+        styleUrls: ['hero-styles.css'],
+        template: `
+          <h1>{{title}}</h1>
+          <h2>My Heroes</h2>
+          <ul class="heroes">
+            <li *ngFor="let hero of heroes"
+                [class.selected]="hero === selectedHero"
+                (click)="onSelect(hero)">
+              <span class="badge">{{hero.id}}</span> {{hero.name}}
+            </li>
+          </ul>
+
+          <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+          `,
+        directives: [app.HeroDetailComponent]//,
+        //providers: [app.HeroService]
+      })
+      .Class({
+        constructor: [app.HeroService, function(heroService) {
+          this.title =  'Tour of Heroes';
+          this.selectedHero = null;
+          this.heroService = heroService;
+        }],
+        onSelect: function(hero) {
+          this.selectedHero = hero;
+        },
+        getHeroes: function() {
+          this.heroService.getHeroes()
+            .then(function(heroes) {
+              this.heroes = heroes;
+            }.bind(this))
+            .catch(function(error) {
+              console.log(error);
+            });
+        },
+        ngOnInit: function() {
+          this.getHeroes();
+        }
+      });
+
+})(window.app || (window.app = {}));
